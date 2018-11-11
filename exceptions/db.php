9c51b710ac44ac8dd0f3934189795bc425d9cb4e
Dário Tavares Antunes <tavaresd@tcd.ie>
2018-11-11 21:41:33 +0000

    Add exception for use in invalid models

--- exceptions/db.php
+++ exceptions/db.php
@@ -5,5 +5,6 @@ namespace recipe\orm;
 class RecordNotPersistedException extends \Exception { }
 class RecordNotFoundException extends \Exception { }
 class InvalidIdException extends \Exception { }
+class InvalidModelException extends \Exception { }
 
 ?>
