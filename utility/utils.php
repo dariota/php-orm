25608e0130d0bc6bc095955338f4a10d533e4424
Dário Tavares Antunes <tavaresd@tcd.ie>
2018-11-25 16:50:49 +0000

    Correct boolean validation to enable roundtrip

--- utility/utils.php
+++ utility/utils.php
@@ -17,10 +17,13 @@ function castable_bool(&$input) {
 	if (is_bool($input))
 		return true;
 
+	# accepts ints so it roundtrips correctly
 	switch (strtolower(strval($input))) {
+	case '1':
 	case 'true':
 		$input = true;
 		return true;
+	case '0':
 	case 'false':
 		$input = false;
 		return true;
